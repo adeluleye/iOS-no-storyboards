9c86bc5c2748abea4b158bf4202c3925aafd83f8
adeluleye <adelu4real@gmail.com>
2018-10-04 12:49:54 +0100

    implement navigation back to login from signup if user already have act

--- LoginNSB/Signup/SignupController.swift
+++ LoginNSB/Signup/SignupController.swift
@@ -10,8 +10,36 @@ import UIKit
 
 class SignupController: UIViewController {
     
+    let haveAccountButton: UIButton = {
+        let color = UIColor.rgb(r: 89, g: 156, b: 120)
+        let font = UIFont.systemFont(ofSize: 16)
+        
+        let button = UIButton(type: .system)
+        button.backgroundColor = GREEN_THEME
+        let attributedTitle = NSMutableAttributedString(string: "Already have an account?", attributes: [NSAttributedString.Key.foregroundColor: color, NSAttributedString.Key.font: font])
+        
+        attributedTitle.append(NSAttributedString(string: "Sign In", attributes: [NSAttributedString.Key.foregroundColor: UIColor.white, NSAttributedString.Key.font: font]))
+        
+        button.addTarget(self, action: #selector(signInAction), for: .touchUpInside)
+        
+        button.setAttributedTitle(attributedTitle, for: .normal)
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .yellow
+        view.backgroundColor = GREEN_THEME
+        
+        setupHaveAccountButton()
+    }
+    
+    @objc func signInAction() {
+        navigationController?.popViewController(animated: true)
+    }
+    
+    private func setupHaveAccountButton() {
+        view.addSubview(haveAccountButton)
+        
+        haveAccountButton.anchors(top: nil, topPad: 0, bottom: view.safeAreaLayoutGuide.bottomAnchor, bottomPad: 8, left: view.leftAnchor, leftPad: 0, right: view.rightAnchor, rightPad: 0, height: 20, width: 0)
     }
 }
